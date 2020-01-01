feac91e18a3530841cd6f5a86f0d417c8ef9edf5
Danny <pi@raspberrypi>
2020-01-01 12:08:07 +0000

    Spacemacs theme!

--- init.el
+++ init.el
@@ -12,14 +12,21 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (evil-mode 1)
-(load-theme 'cyberpunk t)
+;;(load-theme 'cyberpunk t)
+(load-theme 'spacemacs-dark t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (cyberpunk-theme web-mode omnisharp evil undo-tree))))
+ '(custom-safe-themes
+   (quote
+    ("6bc387a588201caf31151205e4e468f382ecc0b888bac98b2b525006f7cb3307" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(fci-rule-color "#383838")
+ '(package-selected-packages
+   (quote
+    (spacemacs-theme cyberpunk-theme web-mode omnisharp evil undo-tree))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
