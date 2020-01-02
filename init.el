1fd0fc6accbba609c8ae650e3485440f69b6f01f
Danny <pi@raspberrypi>
2020-01-02 02:17:57 +0000

    Web mode, PHP file rule

--- init.el
+++ init.el
@@ -22,16 +22,25 @@
 ;; Managed packages
 (use-package undo-tree)
 (use-package evil)
+(use-package moe-theme)
+(use-package web-mode)
 
 ;; Mods
 (evil-mode 1)
+(load-theme 'moe-dark t)
+
+;;Files
+(add-to-list 'auto-mode-alist
+	     '("\\.php\\'" . web-mode))
+
+(add-hook 'web-mode 'display-line-numbers-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (web-mode doom-themes use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
