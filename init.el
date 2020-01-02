dc420932d7e8ad249384ac08e7725f27bd0fcb13
Danny <danny@sdgcore.com>
2020-01-02 03:52:40 -0800

    Backups directory

--- init.el
+++ init.el
@@ -28,7 +28,10 @@
 ;; Mods
 (evil-mode 1)
 (load-theme 'moe-dark t)
-(tool-bar-mode -1)
+(setq backup-directory-alist
+      `((".*" . "~/.backups-emacs")))
+;;(setq auto-save-file-name-transforms
+;;      `((".*" "~/.saves-emacs/" t)))
 
 ;;Files
 (add-to-list 'auto-mode-alist
@@ -41,7 +44,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (web-mode doom-themes use-package))))
+ '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
